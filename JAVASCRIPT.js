9e2381cafd943eb674ed16afa8fe9bc9bc7faf69
Hassan <sasakihaise12012@gmail.com>
2026-06-20 21:12:22 +0300

    Copied usable contents from simple-crud to here such as register code

---
+++ JAVASCRIPT.js
@@ -0,0 +1,58 @@
+
+async function login(){
+
+const username = document.getElementById("username").value;
+const password = document.getElementById("password").value;
+
+const body_sent = {
+
+name: username,
+pw: password
+
+};
+
+try{
+
+const response = await fetch(`http://localhost:3000/login`,{
+
+method: "POST",
+headers: {"Content-Type": "application/json"},
+body: JSON.stringify(body_sent)
+});
+
+}catch(error){
+
+console.log("Error at javascript regarding login", error);
+
+}
+}
+
+
+async function register(){
+
+const username = document.getElementById("new_username").value;
+const password = document.getElementById("new_password").value;
+
+const body_sent = {
+
+name: username,
+pw: password
+
+};
+console.log("name recieved at JAVASCRIPT side?:", username);
+try{
+
+const response = await fetch(`http://localhost:3000/register`,{
+
+method: "POST",
+headers: {"Content-Type": "application/json"},
+body: JSON.stringify(body_sent)
+});
+
+}catch(error){
+
+console.log("Error at javascript regarding registration", error);
+
+}
+
+}
---
+++ db.js
@@ -0,0 +1,14 @@
+const path = require('path');
+require('dotenv').config({path: path.resolve(__dirname, './.env') });
+const { Pool }= require('pg');
+
+const pool = new Pool({
+
+	user: process.env.DB_USER,
+	host: process.env.DB_HOST,
+	database: process.env.DB_NAME,
+	password: process.env.DB_PASSWORD,
+	port: process.env.DB_PORT,
+});
+
+module.exports = pool;
